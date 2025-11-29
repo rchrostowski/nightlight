4310f725f0f02e3874a5e95eaa165016e182685c
rchrostowski <144856089+rchrostowski@users.noreply.github.com>
2025-11-29 01:09:30 -0500

    Update get_factors.py

--- src/get_factors.py
+++ src/get_factors.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from pandas_datareader import famafrench
-from pathlib import Path
 from src.config import FACTORS_DIR
 
 def get_factors():
-    ff = famafrench.FamaFrenchReader("F-F_Research_Data_Factors", start="2010").read()[0]
-    ff = ff.reset_index().rename(columns={"Date":"date"})
+    ff = famafrench.FamaFrenchReader("F-F_Research_Data_Factors", start="2013").read()[0]
+    ff = ff.reset_index().rename(columns={"Date": "date"})
     FACTORS_DIR.mkdir(parents=True, exist_ok=True)
-    ff.to_csv(FACTORS_DIR / "ff_factors.csv", index=False)
-    print("Saved:", FACTORS_DIR / "ff_factors.csv")
+    outpath = FACTORS_DIR / "ff_factors.csv"
+    ff.to_csv(outpath, index=False)
+    print(f"Saved {outpath}")
 
 if __name__ == "__main__":
     get_factors()
