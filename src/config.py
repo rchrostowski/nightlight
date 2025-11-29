cf68d383f3d54236eb62b40fa22eae54b2d14055
rchrostowski <144856089+rchrostowski@users.noreply.github.com>
2025-11-29 01:08:15 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,16 +1,14 @@
 from pathlib import Path
 
 # config.py is inside /src
-# Project root = one level up from /src
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
-DATA_RAW = PROJECT_ROOT / "data_raw"
-DATA_PROCESSED = PROJECT_ROOT / "data_processed"
+DATA_RAW        = PROJECT_ROOT / "data_raw"
+DATA_PROCESSED  = PROJECT_ROOT / "data_processed"
 
-NIGHTLIGHTS_DIR = DATA_RAW / "nightlights"
 HQ_DIR          = DATA_RAW / "hq"
 RETURNS_DIR     = DATA_RAW / "returns"
 FACTORS_DIR     = DATA_RAW / "factors"
+NIGHTLIGHTS_DIR = DATA_RAW / "nightlights"
 
-# Your working panel file location
 FIRM_MONTH_PANEL = DATA_PROCESSED / "firm_month_panel.csv"
